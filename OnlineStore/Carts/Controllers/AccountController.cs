8a24bc9cb5545f0aba1de9e89a02af9008af99f4
asdfasdfasdfasdffff <h1274568231@gmail.com>
2018-06-01 11:37:15 +0800

    123

--- OnlineStore/Carts/Controllers/AccountController.cs
+++ OnlineStore/Carts/Controllers/AccountController.cs
@@ -72,7 +72,7 @@ namespace Carts.Controllers
                 return View(model);
             }
 
-            // 這不會計算為帳戶鎖定的登入失敗
+            // 這不會計算為帳戶鎖定的登入失敗.
             // 若要啟用密碼失敗來觸發帳戶鎖定，請變更為 shouldLockout: true
             var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
             switch (result)
